913aaa7fe066eb3e5dcb0b9f8bad586731bd59e5
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-11-22 15:04:03 -0800

    creates scaffolding for Layout.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
 }
