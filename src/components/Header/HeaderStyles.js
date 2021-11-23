ebe403bee9c285f06bcddd0e714de434c344bfe0
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-11-22 16:02:08 -0800

    creates scaffolding for HeaderStyles.js

---
+++ src/components/Header/HeaderStyles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Div1 = styled.div``;
+
+export const Div2 = styled.div``;
+
+export const NavLink = styled.a``;
