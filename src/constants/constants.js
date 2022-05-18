1b22d0961729122f0f1e42a62c16920b15f74eeb
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-18 16:01:27 -0700

    creates constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: "Metronome",
+    description:
+      "A metronome app built using React and styled components. Has the ability to change both tempo and time signature.",
+    image: null,
+    tags: ["React", "Styled Components"],
+    visit: "https://scanlan-metronome-app.netlify.app/",
+    source: "https://github.com/joshuascan/metronome",
+    id: 0,
+  },
+  {
+    title: "Metronome",
+    description:
+      "A metronome app built using React and styled components. Has the ability to change both tempo and time signature.",
+    image: null,
+    tags: ["React", "Styled Components"],
+    visit: "https://scanlan-metronome-app.netlify.app/",
+    source: "https://github.com/joshuascan/metronome",
+    id: 1,
+  },
+  {
+    title: "Metronome",
+    description:
+      "A metronome app built using React and styled components. Has the ability to change both tempo and time signature.",
+    image: null,
+    tags: ["React", "Styled Components"],
+    visit: "https://scanlan-metronome-app.netlify.app/",
+    source: "https://github.com/joshuascan/metronome",
+    id: 2,
+  },
+  {
+    title: "Metronome",
+    description:
+      "A metronome app built using React and styled components. Has the ability to change both tempo and time signature.",
+    image: null,
+    tags: ["React", "Styled Components"],
+    visit: "https://scanlan-metronome-app.netlify.app/",
+    source: "https://github.com/joshuascan/metronome",
+    id: 3,
+  },
+];
