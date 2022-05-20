90332081d51e688cf70a951902f312121dde64e3
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-19 19:04:59 -0700

    adds technologies component and base text

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,18 @@
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+  SectionText,
+} from "../../styles/GlobalComponents";
+
+const Technologies = () => {
+  return (
+    <Section id="technologies">
+      <SectionDivider />
+      <SectionTitle>Technologies</SectionTitle>
+      <SectionText>
+        I've worked with a range of technologies in the web development world.
+      </SectionText>
+    </Section>
+  );
+};
