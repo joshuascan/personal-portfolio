38cc40f9d9dd680560bacfec7a46f42cfc2f6391
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-20 14:17:35 -0700

    finishes basic structure of Technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,9 +1,17 @@
+import { DiReact, DiDatabase } from "react-icons/di";
 import {
   Section,
   SectionDivider,
   SectionTitle,
   SectionText,
 } from "../../styles/GlobalComponents";
+import {
+  List,
+  ListItem,
+  ListContainer,
+  ListTitle,
+  ListParagraph,
+} from "./TechnologiesStyles";
 
 const Technologies = () => {
   return (
@@ -13,6 +21,30 @@ const Technologies = () => {
       <SectionText>
         I've worked with a range of technologies in the web development world.
       </SectionText>
+      <List>
+        <ListItem>
+          <DiReact size="3rem" />
+          <ListContainer>
+            <ListTitle>Front-End</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              React
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiDatabase size="3rem" />
+          <ListContainer>
+            <ListTitle>Back-End</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              Node
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+      </List>
     </Section>
   );
 };
+
+export default Technologies;
