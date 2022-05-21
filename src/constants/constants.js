4113e4d023ffbed4cb0e71ad1456785c03dcc7f1
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-21 16:20:22 -0700

    changes constants placeholder info

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,37 +4,33 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rutrum justo nisi, luctus posuere dui dictum et. Quisque vulputate nec odio sit amet dignissim. Aenean fermentum feugiat lectus, in pulvinar neque sollicitudin nec.",
     image: "/images/metronome.png",
-    tags: ["React", "Styled Components"],
     visit: "https://scanlan-metronome-app.netlify.app/",
     source: "https://github.com/joshuascan/metronome",
     id: 0,
   },
   {
-    title: "Metronome",
+    title: "Project #2",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rutrum justo nisi, luctus posuere dui dictum et. Quisque vulputate nec odio sit amet dignissim. Aenean fermentum feugiat lectus, in pulvinar neque sollicitudin nec.",
     image: "/images/metronome.png",
-    tags: ["React", "Styled Components"],
     visit: "https://scanlan-metronome-app.netlify.app/",
     source: "https://github.com/joshuascan/metronome",
     id: 1,
   },
   {
-    title: "Metronome",
+    title: "Project #3",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rutrum justo nisi, luctus posuere dui dictum et. Quisque vulputate nec odio sit amet dignissim. Aenean fermentum feugiat lectus, in pulvinar neque sollicitudin nec.",
     image: "/images/metronome.png",
-    tags: ["React", "Styled Components"],
     visit: "https://scanlan-metronome-app.netlify.app/",
     source: "https://github.com/joshuascan/metronome",
     id: 2,
   },
   {
-    title: "Metronome",
+    title: "Project #4",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rutrum justo nisi, luctus posuere dui dictum et. Quisque vulputate nec odio sit amet dignissim. Aenean fermentum feugiat lectus, in pulvinar neque sollicitudin nec.",
     image: "/images/metronome.png",
-    tags: ["React", "Styled Components"],
     visit: "https://scanlan-metronome-app.netlify.app/",
     source: "https://github.com/joshuascan/metronome",
     id: 3,
