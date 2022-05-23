3e73acdb60456550f5d71b7da938c7fcea036fbf
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-23 14:09:43 -0700

    adds validation to contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -21,26 +21,67 @@ const initialFormValues = {
 
 const ContactForm = () => {
   const [message, setMessage] = useState(initialFormValues);
+  const [errors, setErrors] = useState({});
+  const [buttonText, setButtonText] = useState("Send");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showFailureMessage, setShowFailureMessage] = useState(false);
 
   const handleChange = (e) => {
     setMessage({ ...message, [e.target.name]: e.target.value });
   };
 
+  const handleValidation = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (message.name.length <= 0) {
+      tempErrors["name"] = true;
+      isValid = false;
+    }
+    if (message.email.length <= 0) {
+      tempErrors["email"] = true;
+      isValid = false;
+    }
+    if (message.subject.length <= 0) {
+      tempErrors["subject"] = true;
+      isValid = false;
+    }
+    if (message.content.length <= 0) {
+      tempErrors["content"] = true;
+      isValid = false;
+    }
+
+    setErrors({ ...tempErrors });
+    console.log("errors", errors);
+    return isValid;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("/api/sendgrid", {
-      body: JSON.stringify(message),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    });
+    let isValidForm = handleValidation();
 
-    const { error } = await res.json();
-    if (error) {
-      console.log(error);
-      return;
+    if (isValidForm) {
+      setButtonText("Sending");
+      const res = await fetch("/api/sendgrid", {
+        body: JSON.stringify(message),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
+
+      const { error } = await res.json();
+      if (error) {
+        console.log(error);
+        setShowSuccessMessage(false);
+        setShowFailureMessage(true);
+        setButtonText("send");
+        return;
+      }
+      setShowSuccessMessage(true);
+      setShowFailureMessage(false);
+      setButtonText("Send");
     }
     // setMessage(initialFormValues);
   };
@@ -61,6 +102,7 @@ const ContactForm = () => {
               value={message.name}
               onChange={handleChange}
             />
+            {errors?.name && <p>Name cannot be empty</p>}
           </Div1>
           <Div2>
             <label htmlFor="email" />
@@ -73,6 +115,7 @@ const ContactForm = () => {
               value={message.email}
               onChange={handleChange}
             />
+            {errors?.email && <p>Email address cannot be empty</p>}
           </Div2>
         </SenderContainer>
         <label htmlFor="subject" />
@@ -84,6 +127,7 @@ const ContactForm = () => {
           value={message.subject}
           onChange={handleChange}
         />
+        {errors?.subject && <p>Subject cannot be empty</p>}
         <label htmlFor="content" />
         <TextArea
           name="content"
@@ -94,7 +138,12 @@ const ContactForm = () => {
           value={message.content}
           onChange={handleChange}
         />
-        <SubmitButton type="submit">Send</SubmitButton>
+        {errors?.content && <p>Message body cannot be empty</p>}
+        <SubmitButton type="submit">{buttonText}</SubmitButton>
+        {showSuccessMessage && <p>Your message has been delivered.</p>}
+        {showFailureMessage && (
+          <p>Oops! Something went wrong, please try again.</p>
+        )}
       </Form>
     </Section>
   );
