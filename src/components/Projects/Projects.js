a480430b8a26e18305af14fa3c9f20dba7936362
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-06-09 22:36:37 -0700

    changes size of placeholder images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -34,7 +34,7 @@ const Projects = () => {
             return (
               <ProjectContainer key={id} id={id}>
                 <ImageContainer>
-                  <Img src={"https://picsum.photos/300/200"} />
+                  <Img src={"https://picsum.photos/600/400"} />
                 </ImageContainer>
                 <InfoContainer id={id}>
                   <Header>{title}</Header>
