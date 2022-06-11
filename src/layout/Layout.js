3310661ddb7844726711a664af424ed1fe26298a
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-06-11 01:40:30 -0700

    adds meta tags to head

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -9,9 +9,22 @@ export const Layout = ({ children }) => {
   return (
     <>
       <Head>
+        <title>Josh Scanlan | Web Developer</title>
+        <meta
+          name="description"
+          content="Portfolio site of Josh Scanlan. Experience includes React/Redux, Next.js, Ethers, Styled Components, Tailwind CSS, Node, Express, and Solidity."
+        />
+        <link rel="canonical" href="https://joshscanlan.dev/" />
+        <meta property="og:title" content="Josh Scanlan | Web Developer" />
+        <meta property="og:url" content="https://joshscanlan.dev/" />
+        <meta property="og:image" content="/images/profile_pic_large.jpeg" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Web developer experienced in React/Redux, Next.js, Ethers, Styled Components, Tailwind CSS, Node, Express, and Solidity."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <title>Josh Scanlan</title>
       </Head>
       <Container>
         <Header />
